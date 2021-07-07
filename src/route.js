394e2ba8e69559666ef1cb850a182f72c4b927b7
Ronald Assis <ronaldassis71@gmail.com>
2021-07-07 12:03:10 -0300

    Adding database

--- src/route.js
+++ src/route.js
@@ -10,8 +10,8 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 
 route.get('/room/:room', (req, res,) => res.render("room"));
 
-route.post('/room/:room/:question/:action', QuestionController.index)
-route.post('/room/create-room', RoomController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 
 module.exports = route
