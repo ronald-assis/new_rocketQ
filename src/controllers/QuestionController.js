62843ec765428eaa06ae6fa2017d25eeda9ae4c7
Ronald Assis <ronaldassis71@gmail.com>
2021-07-07 18:52:28 -0300

    Database adjustments

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,10 +1,43 @@
-module.exports={
-    index(req, res){
+const Database = require('../db/config')
+
+module.exports = {
+    async index(req, res) {
+        const db = await Database()
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
+        
+        const veryfyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if(veryfyRoom.pass == password){
+            if(action == "delete"){
 
-        console.log(`roomId = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password}`);
+            await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+
+            }else if(action == "check"){
+
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+            }
+        }
+        res.redirect(`/room/${roomId}`)
+
+    },
+
+    async create(req, res) {
+        const db = await Database()
+        const question = req.body.question
+        const roomId = req.params.room
+
+        await db.run(`INSERT INTO questions(
+            title,
+            room,
+            read
+        )VALUES(
+            "${question}",
+            ${roomId},
+            0
+        )`)
+
+        res.redirect(`/room/${roomId}`)
     }
 }
--- src/controllers/RoomCrontroller.js
+++ src/controllers/RoomCrontroller.js
@@ -1,25 +1,45 @@
 const Database = require('../db/config')
 
-module.exports={
-    async create(req,res){
+module.exports = {
+    async create(req, res) {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
+        while (isRoom) {
+            /*Gera o numero da sala */
+            for (let i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                    roomId += Math.floor(Math.random() * 10).toString()
+            }
 
-        for(let i = 0; i < 6; i ++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-            roomId += Math.floor(Math.random() * 10).toString()
+            /* Verifica  se esse numero já existe*/
+            const roomExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomExistIds.some(roomExistId => roomExistId === roomId)
+
+            if (!isRoom) {
+                /* Inseri a sala no Database*/
+                await db.run(`INSERT INTO rooms(
+                id,
+                pass
+                ) VALUES(
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
+
         }
-
-        await db.run(`INSERT INTO rooms(
-            id,
-            pass
-        ) VALUES(
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
-
         await db.close()
         res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req, res){
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+
+
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
     }
 }
